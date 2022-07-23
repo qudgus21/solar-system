76180c5c24c65fb2a82fb4d19f1682e3ebfefad5
qudgus21 <hbh4231@gmail.com>
2022-07-23 16:07:01 +0900

    feat: 태양 추가

--- src/main.js
+++ src/main.js
@@ -1,9 +1,18 @@
 import * as THREE from "three";
 import { Particle } from "./Particle";
 import { canvas, scene, renderer, camera } from "./core";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 
 const particle = new Particle({
   count: 1500,
   path: "images/particle.png",
   spread: 50,
+});
+
+//태양 로드
+const loader = new GLTFLoader();
+loader.load("/models/sun/scene.gltf", (gltf) => {
+  const sun = gltf.scene.children[0];
+  console.log(sun.scale.set(0.5, 0.5, 0.5));
+  scene.add(sun);
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,10 @@
 const path = require("path");
 const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
 const webpackMode = process.env.NODE_ENV || "development";
 
 module.exports = {
@@ -15,7 +16,9 @@ module.exports = {
     path: path.resolve("./dist"),
     filename: "[name].min.js",
   },
-  target: ["web", "es5"],
+  // es5로 빌드 해야 할 경우 주석 제거
+  // 단, 이거 설정하면 webpack-dev-server 3번대 버전에서 live reloading 동작 안함
+  // target: ['web', 'es5'],
   devServer: {
     liveReload: true,
   },
@@ -66,8 +69,8 @@ module.exports = {
       patterns: [
         { from: "./src/main.css", to: "./main.css" },
         { from: "./src/images", to: "./images" },
-        // { from: "./src/models", to: "./models" },
-        // { from: "./src/sounds", to: "./sounds" },
+        { from: "./src/models", to: "./models" },
+        // { from: "./src/sounds", to: "./sounds" }
       ],
     }),
   ],
