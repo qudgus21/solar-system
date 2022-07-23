a92ddaf94f1de3f84383cd589915fbbac6da1f50
qudgus21 <hbh4231@gmail.com>
2022-07-23 15:03:23 +0900

    feat: particle 생성

--- src/main.js
+++ src/main.js
@@ -1,32 +1,9 @@
 import * as THREE from "three";
-import { Mesh } from "three";
-import { cm, geo } from "./common";
 import { Particle } from "./Particle";
 import { canvas, scene, renderer, camera } from "./core";
 
-const particle = new Particle({});
-
-const count = 1500;
-const geometry = new THREE.BufferGeometry();
-const positions = new Float32Array(count * 3);
-for (let i = 0; i < positions.length; i++) {
-  positions[i] = (Math.random() - 0.5) * 50;
-}
-geometry.computeBoundingSphere();
-
-let particleTexture = new THREE.TextureLoader().load("images/particle.png");
-
-geometry.setAttribute("position", new THREE.BufferAttribute(positions, 3));
-
-const material = new THREE.PointsMaterial({
-  size: 0.1,
-  color: "white",
-  map: particleTexture,
-  transparent: true,
-  alphaMap: particleTexture,
-  depthWrite: false,
+const particle = new Particle({
+  count: 1500,
+  path: "images/particle.png",
+  spread: 50,
 });
-
-const mesh = new THREE.Points(geometry, material);
-
-scene.add(mesh);
--- src/particle.js
+++ src/particle.js
@@ -1,7 +1,37 @@
+import * as THREE from "three";
+import { scene } from "./core";
+
 export class Particle {
   constructor(info) {
     this.count = info.count;
     this.path = info.path;
-    this.spread = spread;
+    this.spread = info.spread;
+
+    //geometry
+    this.geometry = new THREE.BufferGeometry();
+    this.positions = new Float32Array(this.count * 3);
+    for (let i = 0; i < this.positions.length; i++) {
+      this.positions[i] = (Math.random() - 0.5) * this.spread;
+    }
+    this.geometry.setAttribute(
+      "position",
+      new THREE.BufferAttribute(this.positions, 3)
+    );
+
+    //material
+    this.particleTexture = new THREE.TextureLoader().load(this.path);
+    this.material = new THREE.PointsMaterial({
+      size: 0.1,
+      color: "white",
+      map: this.particleTexture,
+      transparent: true,
+      alphaMap: this.particleTexture,
+      depthWrite: false,
+    });
+
+    //mesh
+    this.mesh = new THREE.Points(this.geometry, this.material);
+
+    scene.add(this.mesh);
   }
 }
